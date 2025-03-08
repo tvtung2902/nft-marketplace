1b75ba7c97d18103c84f1da9800af33ebce8f26c
unknown <123tranvantung456@gmail.com>
2025-03-08 13:38:08 +0700

    update nft detail

--- app/artist/[id]/nft-list.tsx
+++ app/artist/[id]/nft-list.tsx
@@ -4,7 +4,6 @@ import {default as NftListFromComponent} from "@/app/_component/nft/nft-list";
 export default async function NftList({ functionFetchNfts, isScroll = false, bgColorCardContent = 'background'  }: { functionFetchNfts: () => Promise<NFT[]> , isScroll?:boolean
     ,bgColorCardContent?:string
 }) {
-    console.log("color ne: ", bgColorCardContent)
     const ntfs = await functionFetchNfts();
     return (
         <NftListFromComponent nfts={ntfs} isScroll = {isScroll} bgColorCardContent={bgColorCardContent}/>
