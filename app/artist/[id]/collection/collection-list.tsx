952de9a450fa70aec48619ced933925294a41279
Tran Van Tung <123tranvantung456@gmail.com>
2025-03-13 22:40:06 +0700

    change

--- app/artist/[id]/collection/collection-list.tsx
+++
@@ -1,9 +0,0 @@
-import { getCollectionByCreator } from "@/app/_service/collectionService";
-import { default as CollectionListFromComponent } from "@/app/_component/collection/collection-list";
-
-export default async function CollectionList({ creatorId }: { creatorId: number }) {
-    const collections = await getCollectionByCreator(Number(creatorId));
-    return (
-        <CollectionListFromComponent collections={collections} />
-    )
-}
